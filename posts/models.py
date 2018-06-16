1998b30931d3d6881c3506051a3c5df377e2d5c6
unix2315 <unix.2315@gmail.com>
2018-06-16 19:08:33 +0300

    Add like model, migrations

--- posts/models.py
+++ posts/models.py
@@ -17,3 +17,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,
+                             related_name='likes',
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,
+                             related_name='likes',
+                             on_delete=models.CASCADE)
+    created_at = models.DateTimeField(
+        auto_now_add=True, null=True
+    )
