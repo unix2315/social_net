33bbf1b855495204f083adf8ace99ee980e06226
unix2315 <unix.2315@gmail.com>
2018-06-16 19:04:28 +0300

    Add post model, add migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User,
+                               related_name='posts',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(
+        auto_now_add=True, null=True
+    )
+
+    def __str__(self):
+        return self.title
