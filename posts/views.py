590f9d51560712d09f44738a14d9121ba55006ce
unix2315 <unix.2315@gmail.com>
2018-06-25 08:18:10 +0300

    Add Authentication

--- posts/views.py
+++ posts/views.py
@@ -5,18 +5,22 @@ from posts.models import Post, Like
 from posts.serializers import PostSerializer, LikeSerializer
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import permissions
 
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class AddLike(generics.CreateAPIView, mixins.DestroyModelMixin):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
--- social_net/urls.py
+++ social_net/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^', include('posts.urls')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
